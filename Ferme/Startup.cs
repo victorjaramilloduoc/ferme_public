1220ba941652477164b685f5d2b3053e9b22c862
Fabiola Orellana <fab.orellanag@alumnos.duoc.cl>
2020-04-20 21:32:57 -0400

    Configuración cliente backend en archivo startup

--- Ferme/Startup.cs
+++ Ferme/Startup.cs
@@ -12,6 +12,7 @@ using Ferme.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using FermeBackend;
 
 namespace Ferme
 {
@@ -34,6 +35,7 @@ namespace Ferme
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddHttpClient<api_docsClient>("FermeBackendClient");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
