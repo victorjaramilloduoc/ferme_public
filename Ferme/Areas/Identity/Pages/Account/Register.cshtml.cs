8510b69baf45f460eb38e8bbe8796f70b0ad6c19
esteban jara <es.jara@alumnos.duoc.cl>
2020-04-24 12:57:11 -0400

    creacion de la interface de la pagina registrar

--- Ferme/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Ferme/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -61,6 +61,8 @@ namespace Ferme.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+
         }
 
         public async Task OnGetAsync(string returnUrl = null)
