e200a5f4c0d2c53908442c7d78badda2eeaeab07
esteban jara <es.jara@alumnos.duoc.cl>
2020-05-19 20:41:12 -0400

    añadimos las secciones para carrito de compras incluyendo vista de carrito vacio, tambien añadimos primeras dunciones con js para agregar productos al carrito de compras

---
+++ Ferme/Pages/Carrito/Carrito_compra.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ferme.Pages.Carrito
+{
+    public class Carrito_compraModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
